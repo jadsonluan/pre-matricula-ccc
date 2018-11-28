fcf5852906f6a71497b7a4d73e3ab737a33bcee9
Luan <jadsonluan1@gmail.com>
2018-11-28 13:00:11 -0300

    Finaliza v1

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-import {disciplinas} from './computacao.js'
+import {disciplinas, pre_matricula, add_disciplina, remove_disciplina} from "./computacao.js";
 
-function show_fluxograma(disciplinas) {
-    let $fluxograma = document.querySelector("#fluxograma");
-    let $disciplina;
-    for (var i = 0; i < disciplinas.length; i++) {
-        var disciplina = disciplinas[i];
-        $disciplina = document.createElement("div");
-        $disciplina.style.gridColumn = disciplina.periodo;
-        $disciplina.classList.add('disciplina');
-        $disciplina.innerHTML = `
-        <p class="nome"><b>${disciplina.nome}</b></p>
-        <div class="dados">
-        <span>Créditos: ${disciplina.creditos}</span>
-        <span>Período: ${disciplina.periodo}</span>
-        </div>`;
+const app = (function() {
+    function show_fluxograma(_disciplinas) {
+        let $fluxograma = document.querySelector("#fluxograma");
+        let $disciplina;
+        for (var i = 0; i < _disciplinas.length; i++) {
+            var disciplina = _disciplinas[i];
+            $disciplina = document.createElement("div");
+            $disciplina.style.gridColumn = disciplina.periodo;
+            $disciplina.classList.add("disciplina");
+            
+            $disciplina.onclick = (function(disciplina, $disciplina) {
+                return function() {
+                    click_disciplina(disciplina, $disciplina);
+                };
+            })(disciplina, $disciplina);
 
-        $fluxograma.appendChild($disciplina);
+            $disciplina.innerHTML = `
+            <p class="nome"><b>${disciplina.nome}</b></p>
+            <div class="dados">
+            <span>Créditos: ${disciplina.creditos}</span>
+            <span>Período: ${disciplina.periodo}</span>
+            </div>`;
+
+            $fluxograma.appendChild($disciplina);
+        }
+    };
+
+    function click_disciplina(disciplina, $disciplina) {
+        if ($disciplina.classList.contains("marcada")) 
+            remove_disciplina(disciplina);
+        else
+            add_disciplina(disciplina);
+
+        $disciplina.classList.toggle("marcada");
+        console.log(pre_matricula())
     }
-};
 
-show_fluxograma(disciplinas());
+    show_fluxograma(disciplinas());
+})();
