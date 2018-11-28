72dfc8218fe56740029de5666befdd9c1edb6d0a
Luan <jadsonluan1@gmail.com>
2018-11-28 13:19:52 -0300

    Melhora a modularização

--- app.js
+++ app.js
@@ -2,30 +2,39 @@ import {disciplinas, pre_matricula, add_disciplina, remove_disciplina} from "./c
 
 const app = (function() {
     function show_fluxograma(_disciplinas) {
-        let $fluxograma = document.querySelector("#fluxograma");
-        let $disciplina;
+        var $fluxograma = document.querySelector("#fluxograma");
+        var $disciplina, disciplina;
+
         for (var i = 0; i < _disciplinas.length; i++) {
-            var disciplina = _disciplinas[i];
-            $disciplina = document.createElement("div");
-            $disciplina.style.gridColumn = disciplina.periodo;
-            $disciplina.classList.add("disciplina");
-            
+            disciplina = _disciplinas[i];
+            $disciplina = gera_visualizacao(disciplina);
+
             $disciplina.onclick = (function(disciplina, $disciplina) {
                 return function() {
                     click_disciplina(disciplina, $disciplina);
                 };
             })(disciplina, $disciplina);
 
-            $disciplina.innerHTML = `
-            <p class="nome"><b>${disciplina.nome}</b></p>
-            <div class="dados">
-            <span>Créditos: ${disciplina.creditos}</span>
-            <span>Período: ${disciplina.periodo}</span>
-            </div>`;
-
             $fluxograma.appendChild($disciplina);
         }
     };
+
+    function gera_visualizacao(disciplina) {
+        var $disciplina = document.createElement("div");
+        $disciplina.style.gridColumn = disciplina.periodo;
+        $disciplina.classList.add("disciplina");
+
+        var template = `
+            <p class="nome"><b>${disciplina.nome}</b></p>
+            <div class="dados">
+                <span>Créditos: ${disciplina.creditos}</span>
+                <span>Período: ${disciplina.periodo}</span>
+            </div>
+        `;
+
+        $disciplina.innerHTML = template;
+        return $disciplina;
+    }
 
     function click_disciplina(disciplina, $disciplina) {
         if ($disciplina.classList.contains("marcada")) 
@@ -34,7 +43,6 @@ const app = (function() {
             add_disciplina(disciplina);
 
         $disciplina.classList.toggle("marcada");
-        console.log(pre_matricula())
     }
 
     show_fluxograma(disciplinas());
